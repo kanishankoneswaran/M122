a9a4adcb34994f0485fe0d0e087e950e46736367
kanishankoneswaran <91072363+kanishankoneswaran@users.noreply.github.com>
2022-07-11 15:10:30 +0200

    skriptupdate

--- 02_Meilenstein_B/weather.py
+++ 02_Meilenstein_B/weather.py
@@ -1,20 +1,43 @@
+#############################################################
+
+#FileName:          weather.py
+
+#Crated:            08.07.2022
+
+#Last Edited:       10.07.2022
+
+#Version:           v1.0
+
+#Creater:           Kanishan Koneswaran
+
+#Short-Description: Wetterdaten von Städten auswerten
+
+#Language:          Python
+
+#############################################################
+
+
 import requests
 
+# Variable für den API-Key - requested von openweathermap.org
 API_KEY = "38c055954fd778d52684a3075a05afba"
-# Variable für den API-Key
-city = input("Willkommen! Bitte gib deine gewünschte Stadt ein.")
-# Begrüssung vom Skript und Aufforderung eine Stadt einzugeben
 
+# Begrüssung vom Skript und Aufforderung eine Stadt einzugeben - gewünschte Stadt als Variable festgelegt
+city = input("Willkommen! Bitte gib deine gewünschte Stadt ein.")
+
+# Die URL vom API-Wetterdienst - von hier nimmt ds Skript die nötigen Informationen wie Temperatur etc.
 url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
-# Die URL vom API-Wetterdienst
+
+# Daten von der URL holen - .json bringt die Informationen in die richtige Form
 data = requests.get(url).json()
-# Daten von der URL holen
+
+
+# Variablen für die Temperatur, Luftfeuchtigkeit, Höchsttemperatur und die niedrigste Temperatur - Die Informationen werden von der API geholt und als Variable festgelegt
 temp = data['main']['temp']
 humidity = data['main']['humidity']
 temp_min = data['main']['temp_min']
 temp_max = data['main']['temp_max']
-# Variablen für die Temperatur, Luftfeuchtigkeit, Höchsttemperatur und die niedrigste Temperatur
 
 
+# Ausgabesatz vom Skript mit den nötigen Informationen - Das Skript gibt die verlangen Informaionen in vollständigen Sätzen aus. Hierfür werden die festgelegten Variabeln verwendet.
 print(f'In {city} beträgt die Temperatur {temp} Grad. Die Luftfeuchtigkeit beträgt {humidity} Prozent. Die Höchsttemperatur liegt bei {temp_max} Grad und die niedrigste Temperatur bei {temp_min} Grad.')
-# Ausgabesatz vom Skript mit den nötigen Informationen
